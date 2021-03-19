8c59021c5f3450e626ff5df6ff6383867c18f368
Andrew <healeycodes@gmail.com>
2021-03-19 21:48:13 +0000

    Add command line UI

---
+++ test/test_ui.py
@@ -0,0 +1,23 @@
+from unittest.mock import patch
+import chess
+import unittest
+from ui import render, get_move
+
+
+class TestUI(unittest.TestCase):
+    def test_render_displays_all_characters(self):
+        board = chess.Board(chess.STARTING_FEN)
+        black_pieces = '♖♘♗♕♔♙'
+        white_pieces = '♜♞♝♛♚♟'
+        ranks = '12345678'
+        files = 'abcdefgh'
+
+        display = render(board)
+        for char in black_pieces + white_pieces + ranks + files:
+            self.assertIn(char, display)
+
+    @patch('ui.input', return_value='e2e4')
+    def test_get_move(self, input):
+        board = chess.Board(chess.STARTING_FEN)
+        legal_move = get_move(board)
+        self.assertIn(legal_move, list(board.legal_moves))
---
+++ ui.py
@@ -0,0 +1,77 @@
+# inspired by the https://github.com/thomasahle/sunfish user inferface
+
+import chess
+import argparse
+from movegeneration import next_move
+
+
+def start():
+    '''
+    Start the command line user interface.
+    '''
+    board = chess.Board()
+    user_side = chess.WHITE if input(
+        'Start as [w]hite or [b]lack:\n') == 'w' else chess.BLACK
+
+    if user_side == chess.WHITE:
+        print(render(board))
+        board.push(get_move(board))
+
+    while not board.is_game_over():
+        board.push(next_move(get_depth(), board, debug_info=False))
+        print(render(board))
+        board.push(get_move(board))
+
+    print(f'\nResult: [w] {board.result()} [b]')
+
+
+def render(board: chess.Board) -> str:
+    '''
+    Print a side-relative chess board with special chess characters.
+    '''
+    board_string = list(str(board))
+    uni_pieces = {'R': '♜', 'N': '♞', 'B': '♝', 'Q': '♛', 'K': '♚', 'P': '♟',
+                  'r': '♖', 'n': '♘', 'b': '♗', 'q': '♕', 'k': '♔', 'p': '♙', '.': '·'}
+    for idx, char in enumerate(board_string):
+        if char in uni_pieces:
+            board_string[idx] = uni_pieces[char]
+    ranks = ['1', '2', '3', '4', '5', '6', '7', '8']
+    display = []
+    for rank in ''.join(board_string).split('\n'):
+        display.append(f'  {ranks.pop()} {rank}')
+    if board.turn == chess.BLACK:
+        display.reverse()
+    display.append('    a b c d e f g h')
+    return '\n' + '\n'.join(display)
+
+
+def get_move(board: chess.Board, legal_reminder=False) -> chess.Move:
+    '''
+    Try (and keep trying) to get a legal next move from the user.
+    Play the move by mutating the game board.
+    '''
+    move = input(
+        f'\nEnter a{" _legal_" if legal_reminder else ""} move like {list(board.legal_moves)[0]}:\n')
+
+    for legal_move in board.legal_moves:
+        if move == str(legal_move):
+            return legal_move
+    return get_move(board, legal_reminder=True)
+
+
+def get_depth() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--depth',
+        default=3,
+        help='provide an integer (default: 3)'
+    )
+    args = parser.parse_args()
+    return max([1, int(args.depth)])
+
+
+if __name__ == '__main__':
+    try:
+        start()
+    except KeyboardInterrupt:
+        pass
